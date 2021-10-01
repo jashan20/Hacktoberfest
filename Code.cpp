5968e42afe4c6230878d3306b3b0fb3f39cd4b7f
jashan20 <85362997+jashan20@users.noreply.github.com>
2021-10-01 21:38:01 +0530

    Another commit

---
+++ Code.cpp
@@ -0,0 +1,26 @@
+vector <int> bottomView(Node *root)
+{
+   // Your Code Here
+   vector <int> v;
+   if(root == NULL)
+   return v;
+   
+   queue<pair<Node*,int>> q;
+   map<int,int> m;
+   q.push({root,0});
+   while(!q.empty())
+   {
+       auto p = q.front();
+       Node *curr = p.first;
+       int hd = p.second;
+       q.pop();
+       m[hd] = curr->data;
+       if(curr->left != NULL)
+       q.push({curr->left,hd-1});
+       if(curr->right != NULL)
+       q.push({curr->right,hd+1});
+   }
+   for(auto it = m.begin() ; it != m.end();it++)
+   v.push_back(it->second);
+   return v;
+}
